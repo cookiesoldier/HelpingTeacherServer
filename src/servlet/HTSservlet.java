6e9cb9f6b20d70f9e098890b580094963674ee14
Martin Vieth <martin_123_vieth@hotmail.com>
2016-04-05 12:21:26 +0200

    servelet initializing

---
+++ src/servlet/HTSservlet.java
@@ -0,0 +1,70 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HTSservlet
+ */
+@WebServlet("/HTSservlet")
+public class HTSservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HTSservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	 
+        response.getOutputStream().println("Hurray !! This Servlet Works");
+ 
+    }
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 
+        try {
+            int length = request.getContentLength();
+            byte[] input = new byte[length];
+            ServletInputStream sin = request.getInputStream();
+            int c, count = 0 ;
+            while ((c = sin.read(input, count, input.length-count)) != -1) {
+                count +=c;
+            }
+            sin.close();
+ 
+            String recievedString = new String(input);
+            response.setStatus(HttpServletResponse.SC_OK);
+            OutputStreamWriter writer = new OutputStreamWriter(response.getOutputStream());
+ 
+            Integer doubledValue = Integer.parseInt(recievedString) * 2;
+ 
+            writer.write(doubledValue.toString());
+            writer.flush();
+            writer.close();
+ 
+ 
+ 
+        } catch (IOException e) {
+ 
+ 
+            try{
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.getWriter().print(e.getMessage());
+                response.getWriter().close();
+            } catch (IOException ioe) {
+            }
+        }   
+        }
+ 
+}
