29d84e85528f20d7e156210104f68cb2fa4097fb
Martin Vieth <martin_123_vieth@hotmail.com>
2016-04-11 15:59:11 +0200

    class not found error! gut gut!

--- src/servlet/HTSservlet.java
+++ src/servlet/HTSservlet.java
@@ -10,6 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import firebaseConn.FirebaseConn;
+import net.thegreshams.firebase4j.error.FirebaseException;
+import net.thegreshams.firebase4j.error.JacksonUtilityException;
+
 /**
  * Servlet implementation class HTSservlet
  */
@@ -30,6 +38,53 @@ public class HTSservlet extends HttpServlet {
         response.getOutputStream().println("Hurray !! This Servlet Works");
  
     }
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			int length = request.getContentLength();
+			
+			if(length > 0){
+			byte[] input = new byte[length];
+			ServletInputStream sin = request.getInputStream();
+			int c, count = 0;
+			while ((c = sin.read(input, count, input.length - count)) != -1) {
+				count += c;
+			}
+			sin.close();
+
+			String recievedString = new String(input);
+			JSONParser parser = new JSONParser();
+			JSONObject recievedData = (JSONObject) parser.parse(recievedString);
+			//hent task:
+			System.out.println("ellooooo");
+			String firebResponse = "NULL";
+			if(recievedData.get("TASK").equals("CREATEUSER")){
+				
+				
+				System.out.println("test henrik og signe");
+				firebResponse = FirebaseConn.firebaseCreateUser(recievedData);
+				System.out.println("test Martin");
+			}
+			System.out.println(recievedString);
+			response.setStatus(HttpServletResponse.SC_OK);
+//			System.out.println("FirebaseRespons: " + firebResponse);
+//			System.out.println("RecievedString: "+ recievedString);
+			OutputStreamWriter writer = new OutputStreamWriter(response.getOutputStream());
+
+			writer.write("Succes");
+			writer.flush();
+			writer.close();}
+
+		} catch (IOException | ParseException | JacksonUtilityException | FirebaseException e) {
+
+			try {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				response.getWriter().print(e.getMessage());
+				response.getWriter().close();
+			} catch (IOException ioe) {
+			}
+		}
+	}
  
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
  
