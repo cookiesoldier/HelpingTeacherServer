24c9f61945d4de09d4a1a67f1c1bdaa4f17d5a4c
Martin Vieth <martin_123_vieth@hotmail.com>
2016-04-12 13:34:28 +0200

    test case

--- src/tests/IFirebaseConnectionTest.java
+++ src/tests/IFirebaseConnectionTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.json.simple.JSONObject;
 import org.junit.Test;
 
 import firebaseConn.FirebaseConnection;
@@ -12,8 +13,23 @@ public class IFirebaseConnectionTest {
 	IFirebaseConnection firebase = new FirebaseConnection();
 	
 	
+	
 	@Test
 	public void testCreateUser() {
+		JSONObject obj = new JSONObject();
+       
+            try {
+				obj.put("TASK", "CREATEUSER");
+				obj.put("USERNAME", "MartinOne");
+				obj.put("PASSONE", "123");
+				obj.put("PASSTWO", "123");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            
+            firebase.createUser(obj);
+      
 		
 	}
 
