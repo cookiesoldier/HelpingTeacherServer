e8a4fdc8bbe8abe1fed84a22e9257972cb000948
Martin Vieth <martin_123_vieth@hotmail.com>
2016-04-21 10:24:46 +0200

    tilføjet metoder til Interface og fjernet gamle klasser

--- src/firebaseConn/IUserDatabase.java
+++ src/firebaseConn/IUserDatabase.java
@@ -2,15 +2,45 @@ package firebaseConn;
 
 import org.json.simple.JSONObject;
 
+import dtos.AnswerDTO;
+import dtos.EventDTO;
+import dtos.QuestionDTO;
+import dtos.RoomDTO;
 import dtos.UserDTO;
 
 public interface IUserDatabase {
-	
-	boolean createUser(UserDTO user);
+
 	boolean authUser(String username, String password);
 	UserDTO getUser(String name);
 	JSONObject userToJSON(UserDTO user);
 	UserDTO JSONtoUser(JSONObject obj);
 	boolean isJSONObjectUser(JSONObject obj);
 	
+	
+
+	boolean createUser(UserDTO user);
+	boolean createAnswer(AnswerDTO answer);
+	boolean createEvent(EventDTO event);
+	boolean createQuestion(QuestionDTO question);
+	boolean createRoom(RoomDTO room);
+	
+	
+	
+	boolean deleteRoom(RoomDTO room);
+	boolean deleteEvent(EventDTO event);
+	boolean deleteQuestion(QuestionDTO question);
+	boolean deleteAnswers(AnswerDTO answer);
+	boolean deleteUser(UserDTO user);
+	
+	boolean updateRoom(RoomDTO room);
+	boolean updateEvent(EventDTO event);
+	boolean updateQuestion(QuestionDTO question);
+	boolean updateAnswers(AnswerDTO answer);
+	boolean updateUser(UserDTO user);
+	
+	boolean getRoom(RoomDTO room);
+	boolean getEvent(EventDTO event);
+	boolean getQuestion(QuestionDTO question);
+	boolean getAnswers(AnswerDTO answer);
+	boolean getUser(UserDTO user);
 }
