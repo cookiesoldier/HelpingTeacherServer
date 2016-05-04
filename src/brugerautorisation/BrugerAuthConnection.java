44433976df488594a596e3843cc373958c623318
thomas <thomas@liljegren.dk>
2016-05-04 11:37:22 +0200

    rundtrykning

--- src/brugerautorisation/BrugerAuthConnection.java
+++ src/brugerautorisation/BrugerAuthConnection.java
@@ -1,4 +1,4 @@
-package rmi;
+package brugerautorisation;
 
 import java.net.MalformedURLException;
 import java.rmi.Naming;
@@ -8,14 +8,14 @@ import java.rmi.RemoteException;
 import brugerautorisation.data.Bruger;
 import brugerautorisation.transport.rmi.Brugeradmin;
 import daos.Handler;
-import daos.impl.UserDAO;
+import daos.interfaces.IUserDAO;
 import dtos.UserDTO;
 import helper.Password;
 import helper.SessionMap;
 
 public class BrugerAuthConnection {
 	
-	UserDAO userdao;
+	IUserDAO userdao;
 	Brugeradmin ba;
 	SessionMap sessions = Handler.sessions;
 	final static String DB_URL = "rmi://javabog.dk/brugeradmin";
@@ -47,7 +47,7 @@ public class BrugerAuthConnection {
 			String passwordToStore;
 			try {
 				b = ba.hentBruger(username, password);
-				passwordToStore = Password.getSaltedHash(username+password); // password der gemmes er hash af username og password fra jakobs database
+				passwordToStore = Password.getSaltedHash(username+password); // password der gemmes er salted hash af username og password fra jakobs database
 				UserDTO user = new UserDTO(b.brugernavn, b.email, b.fornavn,
 						b.efternavn, passwordToStore);
 				sessions.addSession(username, sessionKey); // session key is put into map with username
--- src/daos/Handler.java
+++ src/daos/Handler.java
@@ -5,15 +5,20 @@ import daos.impl.EventDAO;
 import daos.impl.QuestionDAO;
 import daos.impl.RoomDAO;
 import daos.impl.UserDAO;
+import daos.interfaces.IAnswerDAO;
+import daos.interfaces.IEventDAO;
+import daos.interfaces.IQuestionDAO;
+import daos.interfaces.IRoomDAO;
+import daos.interfaces.IUserDAO;
 import helper.SessionMap;
 
 public class Handler {
 	
-	public static UserDAO userdao = new UserDAO();
-	public static EventDAO eventdao = new EventDAO();
-	public static RoomDAO roomdao = new RoomDAO();
-	public static QuestionDAO questiondao = new QuestionDAO();
-	public static AnswerDAO answerdao = new AnswerDAO();
+	public static IUserDAO userdao = new UserDAO();
+	public static IEventDAO eventdao = new EventDAO();
+	public static IRoomDAO roomdao = new RoomDAO();
+	public static IQuestionDAO questiondao = new QuestionDAO();
+	public static IAnswerDAO answerdao = new AnswerDAO();
 	
 	public static SessionMap sessions = new SessionMap();
 	
